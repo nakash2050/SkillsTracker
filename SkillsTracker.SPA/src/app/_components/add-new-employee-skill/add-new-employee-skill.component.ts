4eb14cc527c7130ca7311be67e26b3776f42ddd1
Nakash Sheikh <nakash2050@gmail.com>
2018-05-05 20:51:25 +0530

    Prepared Associate Model for database save

--- SkillsTracker.SPA/src/app/_components/add-new-employee-skill/add-new-employee-skill.component.ts
+++ SkillsTracker.SPA/src/app/_components/add-new-employee-skill/add-new-employee-skill.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { SkillService } from "../../_services/skill.service";
 import { SkillModel } from "../../_models/skill.model";
 import { Router } from "@angular/router";
+import { AssociateModel } from "./../../_models/associate.model";
 
 @Component({
   selector: "app-add-new-employee-skill",
@@ -25,6 +26,50 @@ export class AddNewEmployeeSkillComponent implements OnInit {
 
   submit(form) {
     console.log(form.value);
+
+    const associateData: any = {
+      associateId: form.value.associateId,
+      name: form.value.name,
+      email: form.value.email,
+      mobile: form.value.mobile,
+      remark: form.value.remark,
+      strength: form.value.strength,
+      weakness: form.value.weakness,
+      skills: []
+    };
+
+    switch (form.value.level) {
+      case "L1":
+        associateData.level1 = true;
+        break;
+      case "L2":
+        associateData.level2 = true;
+        break;
+      case "L3":
+        associateData.level3 = true;
+        break;
+    }
+
+    switch (form.value.status) {
+      case "green":
+        associateData.statusGreen = true;
+        break;
+      case "blue":
+        associateData.statusBlue = true;
+        break;
+      case "red":
+        associateData.statusRed = true;
+        break;
+    }
+
+    let skills = form.value.skills;
+    Object.keys(skills).forEach(key => {
+      if (skills[key] != 0) {
+        associateData.skills.push({ skillId: key, skillRating: skills[key] });
+      }
+    });
+
+    console.log(associateData);
   }
 
   reset(form) {
---
+++ SkillsTracker.SPA/src/app/_models/associate.model.ts
@@ -0,0 +1,20 @@
+import { SkillModel } from "./skill.model";
+
+export interface AssociateModel {
+  associateId: any;
+  name: string;
+  email: boolean;
+  mobile: number;
+  picture: string;
+  gender: string;
+  statusGreen: boolean;
+  statusBlue: boolean;
+  statusRed: boolean;
+  level1: boolean;
+  level2: boolean;
+  level3: boolean;
+  remark: string;
+  strength: string;
+  weakness: string;
+  skills: Array<SkillModel>;
+}
