f1ace79419a7f0bf8f7454caca5df155d19d8417
Nakash Sheikh <nakash2050@gmail.com>
2018-05-07 00:07:41 +0530

    Completed Dashboard SP data

--- SkillsTracker.SPA/src/app/_components/dashboard/dashboard.component.ts
+++ SkillsTracker.SPA/src/app/_components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-dashboard',
@@ -24,7 +25,7 @@ export class DashboardComponent implements OnInit {
 
   ngAfterViewInit(): void {
     let html5C = (<HTMLCanvasElement>this.html5Canvas.nativeElement);
-    html5C.width = 300;
+    html5C.width = 106;
     this.context = (<HTMLCanvasElement>this.html5Canvas.nativeElement).getContext('2d');
     this.context.font = '15px "Oswald", sans-serif';
     this.context.fillStyle = "black";
@@ -32,7 +33,7 @@ export class DashboardComponent implements OnInit {
     this.context.fillText('HTML5', html5C.width/2, html5C.height/2);
 
     let css3C = (<HTMLCanvasElement>this.css3Canvas.nativeElement);
-    css3C.width = 100;
+    css3C.width = 353;
     this.context = (<HTMLCanvasElement>this.css3Canvas.nativeElement).getContext('2d');
     this.context.font = '15px "Oswald", sans-serif';
     this.context.fillStyle = 'black';
@@ -40,7 +41,7 @@ export class DashboardComponent implements OnInit {
     this.context.fillText('CSS3', css3C.width/2, css3C.height/2);
 
     let javaC = (<HTMLCanvasElement>this.javaCanvas.nativeElement);
-    javaC.width = 100;
+    javaC.width = 470;
     this.context = (<HTMLCanvasElement>this.javaCanvas.nativeElement).getContext('2d');
     this.context.font = '15px "Oswald", sans-serif';
     this.context.fillStyle = 'black';
@@ -48,7 +49,7 @@ export class DashboardComponent implements OnInit {
     this.context.fillText('Java', javaC.width/2, javaC.height/2);
 
     let springC = (<HTMLCanvasElement>this.springCanvas.nativeElement);
-    springC.width = 100;
+    springC.width = 235;
     this.context = (<HTMLCanvasElement>this.springCanvas.nativeElement).getContext('2d');
     this.context.font = '15px "Oswald", sans-serif';
     this.context.fillStyle = 'black';
@@ -56,7 +57,7 @@ export class DashboardComponent implements OnInit {
     this.context.fillText('Spring', springC.width/2, springC.height/2);
 
     let restfulC = (<HTMLCanvasElement>this.restfulCanvas.nativeElement);
-    restfulC.width = 100;
+    restfulC.width = 0;
     this.context = (<HTMLCanvasElement>this.restfulCanvas.nativeElement).getContext('2d');
     this.context.font = '15px "Oswald", sans-serif';
     this.context.fillStyle = 'black';
@@ -64,7 +65,7 @@ export class DashboardComponent implements OnInit {
     this.context.fillText('Restful', restfulC.width/2, restfulC.height/2);
 
     let angular1C = (<HTMLCanvasElement>this.angular1Canvas.nativeElement);
-    angular1C.width = 100;
+    angular1C.width = 0;
     this.context = (<HTMLCanvasElement>this.angular1Canvas.nativeElement).getContext('2d');
     this.context.font = '15px "Oswald", sans-serif';
     this.context.fillStyle = 'black';
@@ -72,7 +73,7 @@ export class DashboardComponent implements OnInit {
     this.context.fillText('Angular1', angular1C.width/2, angular1C.height/2);
 
     let angular2C = (<HTMLCanvasElement>this.angular2Canvas.nativeElement);
-    angular2C.width = 100;
+    angular2C.width = 235;
     this.context = (<HTMLCanvasElement>this.angular2Canvas.nativeElement).getContext('2d');
     this.context.font = '15px "Oswald", sans-serif';
     this.context.fillStyle = 'black';
@@ -80,14 +81,13 @@ export class DashboardComponent implements OnInit {
     this.context.fillText('Angular2', angular2C.width/2, angular2C.height/2);
 
     let reactC = (<HTMLCanvasElement>this.reactCanvas.nativeElement);
-    reactC.width = 100;
+    reactC.width = 0;
     this.context = (<HTMLCanvasElement>this.reactCanvas.nativeElement).getContext('2d');
     this.context.font = '15px "Oswald", sans-serif';
     this.context.fillStyle = 'black';
     this.context.textAlign = 'center';
     this.context.fillText('React', reactC.width/2, reactC.height/2);
+
+    console.log($('#divContainer').width());
   }
-
-  
-
 }
--- SkillsTracker.StoredProcedures/sp_GetDashboardData.sql
+++ SkillsTracker.StoredProcedures/sp_GetDashboardData.sql
@@ -10,6 +10,116 @@ GO
 CREATE PROCEDURE sp_GetDashboardData
 AS
 BEGIN
+	DECLARE @TotalCandidates INT
+	DECLARE	@TotalNumberOfFemaleCandidates INT
+	DECLARE	@TotalNumberOfMaleCandidates INT
+	DECLARE	@TotalNumberOfFreshers INT
+	DECLARE @TotalNumberOfL1Candidates INT
+	DECLARE @TotalNumberOfL2Candidates INT
+	DECLARE @TotalNumberOfL3Candidates INT
+	DECLARE @TotalNumberOfCandidatesRated INT
+	DECLARE @TotalNumberOfFemaleCandidatesRated INT
+	DECLARE @TotalNumberOfMaleCandidatesRated INT
+	
+	DECLARE @TotalHTML5Candidates INT
+	DECLARE @TotalCSS3Candidates INT
+	DECLARE @TotalJavaCandidates INT
+	DECLARE @TotalCSharpCandidates INT
+	DECLARE @TotalRestfulCandidates INT
+	DECLARE @TotalAngular1Candidates INT
+	DECLARE @TotalAngular2Candidates INT
+	DECLARE @TotalReactCandidates INT
+
+	SELECT @TotalCandidates = COUNT(*) FROM dbo.Associates
+
+	SELECT @TotalNumberOfFemaleCandidates = COUNT(*) FROM dbo.Associates WHERE Gender = 'F'
+
+	SELECT @TotalNumberOfMaleCandidates = COUNT(*) FROM dbo.Associates WHERE Gender = 'M'
+
+	SELECT @TotalNumberOfFreshers = COUNT(*) FROM dbo.Associates WHERE Level_1 = 1
+
+	SELECT @TotalNumberOfCandidatesRated = COUNT(DISTINCT(Associate_ID)) FROM dbo.AssociateSkills
+
+	SELECT @TotalNumberOfFemaleCandidatesRated = COUNT(DISTINCT(ASTS.Associate_ID)) FROM dbo.AssociateSkills ASTS
+		LEFT JOIN dbo.Associates AST ON ASTS.Associate_ID = AST.Associate_ID
+	WHERE AST.Gender = 'F'
+
+	SELECT @TotalNumberOfMaleCandidatesRated = COUNT(DISTINCT(ASTS.Associate_ID)) FROM dbo.AssociateSkills ASTS
+		LEFT JOIN dbo.Associates AST ON ASTS.Associate_ID = AST.Associate_ID
+	WHERE AST.Gender = 'M'
+
+	SELECT @TotalNumberOfL1Candidates = @TotalNumberOfFreshers
+
+	SELECT @TotalNumberOfL2Candidates = COUNT(*) FROM dbo.Associates WHERE Level_2 = 1
+
+	SELECT @TotalNumberOfL3Candidates = COUNT(*) FROM dbo.Associates WHERE Level_3 = 1
+
+	-- For Tiles
+	SELECT
+		@TotalCandidates AS [RegisteredCandidates],
+		CAST(ROUND(CAST(@TotalNumberOfFemaleCandidates*100.0 / @TotalCandidates AS DECIMAL(10,2)), 0) AS INT) AS [PercentageOfFemaleCandidates],
+		CAST(ROUND(CAST(@TotalNumberOfMaleCandidates*100.0 / @TotalCandidates AS DECIMAL(10,2)), 0) AS INT) AS [PercentageOfMaleCandidates],
+		CAST(ROUND(CAST(@TotalNumberOfFreshers*100.0 / @TotalCandidates AS DECIMAL(10,2)), 0) AS INT) AS [PercentageOfFreshers],
+		@TotalNumberOfCandidatesRated AS [CandidatesRated],
+		CAST(ROUND(CAST(@TotalNumberOfFemaleCandidatesRated*100.0 / @TotalNumberOfCandidatesRated AS DECIMAL(10,2)), 0) AS INT) AS [PercentageOfFemaleCandidatesRated],
+		CAST(ROUND(CAST(@TotalNumberOfMaleCandidatesRated*100.0 / @TotalNumberOfCandidatesRated AS DECIMAL(10,2)), 0) AS INT) AS [PercentageOfMaleCandidatesRated],
+		CAST(ROUND(CAST(@TotalNumberOfL1Candidates*100.0 / @TotalCandidates AS DECIMAL(10,2)), 0) AS INT) AS [PercentageOfLevel1Candidates],
+		CAST(ROUND(CAST(@TotalNumberOfL2Candidates*100.0 / @TotalCandidates AS DECIMAL(10,2)), 0) AS INT) AS [PercentageOfLevel2Candidates],
+		CAST(ROUND(CAST(@TotalNumberOfL3Candidates*100.0 / @TotalCandidates AS DECIMAL(10,2)), 0) AS INT) AS [PercentageOfLevel3Candidates]
+
+	-- For Chart
+	SELECT @TotalHTML5Candidates = COUNT(*) FROM dbo.AssociateSkills ASK
+	LEFT JOIN dbo.Skills SK
+		ON ASK.Skill_ID = SK.Skill_ID
+	WHERE SK.Skill_Name LIKE '%HTML5%'
+
+	SELECT @TotalCSS3Candidates = COUNT(*) FROM dbo.AssociateSkills ASK
+	LEFT JOIN dbo.Skills SK
+		ON ASK.Skill_ID = SK.Skill_ID
+	WHERE SK.Skill_Name LIKE '%CSS3%'
+
+	SELECT @TotalJavaCandidates = COUNT(*) FROM dbo.AssociateSkills ASK
+	LEFT JOIN dbo.Skills SK
+		ON ASK.Skill_ID = SK.Skill_ID
+	WHERE SK.Skill_Name LIKE '%Java%'
+
+	SELECT @TotalCSharpCandidates = COUNT(*) FROM dbo.AssociateSkills ASK
+	LEFT JOIN dbo.Skills SK
+		ON ASK.Skill_ID = SK.Skill_ID
+	WHERE SK.Skill_Name LIKE '%C#%'
+
+	SELECT @TotalRestfulCandidates = COUNT(*) FROM dbo.AssociateSkills ASK
+	LEFT JOIN dbo.Skills SK
+		ON ASK.Skill_ID = SK.Skill_ID
+	WHERE SK.Skill_Name LIKE '%Restful%'
+
+	SELECT @TotalAngular1Candidates = COUNT(*) FROM dbo.AssociateSkills ASK
+	LEFT JOIN dbo.Skills SK
+		ON ASK.Skill_ID = SK.Skill_ID
+	WHERE SK.Skill_Name LIKE '%Angular 1%'
+
+	SELECT @TotalAngular2Candidates = COUNT(*) FROM dbo.AssociateSkills ASK
+	LEFT JOIN dbo.Skills SK
+		ON ASK.Skill_ID = SK.Skill_ID
+	WHERE SK.Skill_Name LIKE '%Angular 2%'
+
+	SELECT @TotalReactCandidates = COUNT(*) FROM dbo.AssociateSkills ASK
+	LEFT JOIN dbo.Skills SK
+		ON ASK.Skill_ID = SK.Skill_ID
+	WHERE SK.Skill_Name LIKE '%React%'
+
+
+	SELECT
+		CAST(ROUND(CAST(@TotalHTML5Candidates*100.0 / @TotalNumberOfCandidatesRated AS DECIMAL(10,2)), 0) AS INT) AS [PercentageOfHTML5Candidates],
+		CAST(ROUND(CAST(@TotalCSS3Candidates*100.0 / @TotalNumberOfCandidatesRated AS DECIMAL(10,2)), 0) AS INT) AS [PercentageOfCSS3Candidates],
+		CAST(ROUND(CAST(@TotalJavaCandidates*100.0 / @TotalNumberOfCandidatesRated AS DECIMAL(10,2)), 0) AS INT) AS [PercentageOfJavaCandidates],
+		CAST(ROUND(CAST(@TotalCSharpCandidates*100.0 / @TotalNumberOfCandidatesRated AS DECIMAL(10,2)), 0) AS INT) AS [PercentageOfCSharpCandidates],
+		CAST(ROUND(CAST(@TotalRestfulCandidates*100.0 / @TotalNumberOfCandidatesRated AS DECIMAL(10,2)), 0) AS INT) AS [PercentageOfRestfulCandidates],
+		CAST(ROUND(CAST(@TotalAngular1Candidates*100.0 / @TotalNumberOfCandidatesRated AS DECIMAL(10,2)), 0) AS INT) AS [PercentageOfAngular1Candidates],
+		CAST(ROUND(CAST(@TotalAngular2Candidates*100.0 / @TotalNumberOfCandidatesRated AS DECIMAL(10,2)), 0) AS INT) AS [PercentageOfAngular2Candidates],
+		CAST(ROUND(CAST(@TotalReactCandidates*100.0 / @TotalNumberOfCandidatesRated AS DECIMAL(10,2)), 0) AS INT) AS [PercentageOfReactCandidates]
+
+	--For Table
 	SELECT Associate_ID [AssociateID], AST.Name, AST.Email, AST.Mobile, AST.Picture, 
 	CASE 
 		WHEN AST.Status_Green = 1 THEN 'GREEN'
